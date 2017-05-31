e35008f7773ae46000a22f55387696d8237aabef
konor1010 <konorev_dv@mail.ru>
2017-05-31 15:43:51 +0300

    Добавлена страница клубов

--- src/app/components/children-teams/children-teams.component.ts
+++ src/app/components/children-teams/children-teams.component.ts
@@ -1,17 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 
+export class ChildrenTeam {
+  id: number;
+  name: string;
+  city: string;
+  nation: string;
+  year: number;
+  image: string;
+
+
+}
+
 @Component({
   selector: 'app-children-teams',
   templateUrl: './children-teams.component.html',
   styleUrls: ['./children-teams.component.scss']
 })
 export class ChildrenTeamsComponent implements OnInit {
-
   title = 'Детские команды';
+  childrenteam: ChildrenTeam = {
+    id: 1,
+    name: 'Олимпийские надежды',
+    city: 'Санкт-Петербург',
+    nation: 'Российская Федерация',
+    year: 2008,
+    image: 'assets/img/hoc1.png'
+  }
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
   }
-
+  public childrenteams = CHILDRENTEAMS;
 }
+
+let CHILDRENTEAMS: ChildrenTeam[] = [
+  { "id": 1, "name": "Олимпийские надежды", "city": "Санкт-Петербург", "nation": "Российская Федерация", "year" : 2008, "image": "assets/img/emblem/olimp nadegdi.jpeg"},
+  { "id": 2, "name": "Форвард", "city": "Санкт-Петербург", "nation": "Российская Федерация", "year" : 2008,  "image": "assets/img/emblem/hockey_forvard_spb.jpeg"},
+  { "id": 3, "name": "СКА ХКД", "city": "Санкт-Петербург", "nation": "Российская Федерация", "year" : 2008,  "image": "assets/img/emblem/hockey_ska_hkd_spb.jpeg"},
+  { "id": 4, "name": "Красная Звезда ", "city": "Санкт-Петербург", "nation": "Российская Федерация", "year" : 2008,  "image": "assets/img/emblem/hockey_krasnaya_zvezda_spb.png"},
+  { "id": 5, "name": "Шуйские Соколы", "city": "Шуя", "nation": "Российская Федерация", "year" : 2008, "image": "assets/img/hoc2.png"},
+  { "id": 6, "name": "Короли Льда", "city": "Санкт-Петербург", "nation": "Российская Федерация", "year" : 2008,  "image": "assets/img/emblem/hockey_koroli_lda_spb.png"},
+  { "id": 7, "name": "Бульдоги" , "city": "Санкт-Петербург", "nation": "Российская Федерация", "year" : 2008,"image": "assets/img/emblem/hockey_buldogy_spb.jpg"},
+  { "id": 8, "name": "Йети", "city": "Великий Новгород", "nation": "Российская Федерация", "year" : 2008, "image": "assets/img/emblem/hockey_yety_velikiy_novgorod.jpg"},
+  { "id": 9, "name": "Название", "city": "Санкт-Петербург", "nation": "Российская Федерация", "year" : 2008, "image": "assets/img/hoc2.png"},
+  { "id": 10, "name": "Название", "city": "Санкт-Петербург", "nation": "Российская Федерация", "year" : 2008,  "image": "assets/img/hoc2.png"}
+];
