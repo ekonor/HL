da890609c7deaa05e1d2f0b8bc05355a8747f065
marina <m.tyuleneva@list.ru>
2017-12-14 21:16:55 +0300

    fix: angular2-permission error

--- src/app/core/auth/auth.guard.ts
+++ src/app/core/auth/auth.guard.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import { PermissionService } from 'angular2-permission';
+// import { PermissionService } from 'angular2-permission';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
 
-  constructor(private router: Router, private permissionService: PermissionService) { }
+  // constructor(private router: Router, private permissionService: PermissionService) { }
+  constructor(private router: Router) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean>|boolean {
     const roles = route.data['roles'] as Array<string>;
@@ -18,7 +19,7 @@ export class AuthGuard implements CanActivate {
       for (let i = 0; i < myRoles.length; i++) {
         if (roles.indexOf(myRoles[i]) !== -1) {
           access = true; // someone role
-          this.permissionService.add(myRoles[i]);
+          // this.permissionService.add(myRoles[i]);
         }
       }
       if (access === false) {
