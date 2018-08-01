0a372b6c0dfa5fc5df6bab576f7b233a42c58547
marina <m.tyuleneva@list.ru>
2018-08-01 19:23:07 +0300

    [app/t/t-create] add: division types, tournament

--- src/app/tournaments/tournament-create/tournament-create.component.ts
+++ src/app/tournaments/tournament-create/tournament-create.component.ts
@@ -15,11 +15,11 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AlertService } from 'app/components/alert/alert.service';
 import { TournamentService } from 'app/tournaments/shared/tournament.service';
 
-/*import { TournamentAnnouncement } from 'app/tournament-announcements/shared/tournament-announcement';
+import { Tournament } from 'app/tournaments/shared/tournament';
 import { Item } from 'app/tournament-announcements/shared/item';
 import { City } from 'app/core/geo/city';
 
-import { ArenaListItem } from 'app/arenas/shared/arena-list-item';
+/* import { ArenaListItem } from 'app/arenas/shared/arena-list-item';
 import { Arena } from 'app/arenas/shared/arena';*/
 
 
@@ -30,12 +30,14 @@ import { Arena } from 'app/arenas/shared/arena';*/
   templateUrl: 'tournament-create.component.html'
 })
 export class TournamentCreateComponent implements OnInit {
-  tournament: string;
+  tournament: Tournament;
   returnUrl: string;
   dataIsLoading: boolean;
   errorMessage: string;
 
   step: number;
+  oneDay: boolean;
+  divisionTypes: Array<Item>;
 
   constructor( private tournamentService: TournamentService,
                private router: Router,
@@ -43,9 +45,11 @@ export class TournamentCreateComponent implements OnInit {
                private alertService: AlertService ) {
     this.returnUrl = this.activatedRoute.snapshot.queryParams['returnUrl'] || '/';
     this.dataIsLoading = true;
-    this.tournament = '123';
+    this.tournament = new Tournament();
     this.dataIsLoading = false;
     this.step = 1;
+    this.oneDay = false;
+    this.getDivisionTypes();
   }
 
   ngOnInit() {
@@ -55,5 +59,18 @@ export class TournamentCreateComponent implements OnInit {
     if (step > 0 && step < 4) {
       this.step = step;
     }
+  }
+
+  private getDivisionTypes() {
+    this.divisionTypes = new Array<Item>();
+    this.divisionTypes.push(new Item({value: null, name: 'Нет этапов'}));
+    this.divisionTypes.push(new Item({value: 'A', name: 'Группа "A"'}));
+    this.divisionTypes.push(new Item({value: 'B', name: 'Группа "B"'}));
+    this.divisionTypes.push(new Item({value: 'C', name: 'Группа "C"'}));
+    this.divisionTypes.push(new Item({value: 'D', name: 'Группа "D"'}));
+    this.divisionTypes.push(new Item({value: '1', name: '1 этап'}));
+    this.divisionTypes.push(new Item({value: '2', name: '2 этап'}));
+    this.divisionTypes.push(new Item({value: '3', name: '3 этап'}));
+    this.divisionTypes.push(new Item({value: '4', name: '4 этап'}));
   }
 }
